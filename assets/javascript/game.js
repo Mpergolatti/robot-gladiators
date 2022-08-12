388c4406f61ff3529746ffb76b2af57b8ef71194
Mpergolatti <Mpergolatti@gmail.com>
2022-08-12 14:57:57 -0400

    prompts user to enter name

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,18 @@
-window.alert("this is an alert javascript is running")
+// Asks user what robots name is
+
+var playerName = window.prompt("What is your robot's name?");
+// window.alert(playerName);
+console.log(playerName);
+console.log("this logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// displays text and robots name
+console.log("Our robot's name is, " + playerName);
+
+// Creates function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+fight();
