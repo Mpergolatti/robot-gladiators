648fac017b9ae4817476a48d7fe399533ac1cde2
Mpergolatti <Mpergolatti@gmail.com>
2022-08-12 20:07:02 -0400

    learned for loops

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,11 +11,17 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-console.log(enemyNames);
-console.log(enemyNames[0]);
-console.log(enemyNames[1]);
-console.log(enemyNames[2]);
-console.log(enemyNames.length);
+// console.log(enemyNames);
+// console.log(enemyNames[0]);
+// console.log(enemyNames[1]);
+// console.log(enemyNames[2]);
+// console.log(enemyNames.length);
+
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+}
 
 var fight = function() {
     // Alert palyers that they are starting the round
